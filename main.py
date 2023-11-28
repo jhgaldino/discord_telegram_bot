b15ad93c8f0fd8ab6323628e0dc0c74954841fd9
Joanthan <poliekos@outlook.com>
2023-11-27 22:43:46 -0300

    Filter link added
    Co-authored-by: Jordan <DanGM96@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from telethon import TelegramClient, events
 import os
+import re
 import discord
 from discord.ext import commands
+from telethon import TelegramClient, events
 
 # Inicializa os intents do Discord
 intents = discord.Intents.default()
@@ -21,21 +22,34 @@ telegram_channels = os.environ.get('TELEGRAM_CHANNELS').split(',')
 # Variável global para armazenar o contexto
 global_ctx = None
 
+def filter(event):
+    if re.search(r'https://', event.raw_text):
+        return True
+    return False
+
+def format(event: events.NewMessage.Event):
+    text = re.sub(r"\n+", "\n", event.raw_text)
+    return text
+
+# Exemplo de Teste de eventos
+# @client.on(events.NewMessage(func=filter))
+# async def teste(event):
+#     text = format(event)
+#     print(text)
+
 # Evento para quando o bot do Discord estiver pronto
-@client.on(events.NewMessage(chats=telegram_channels))
+@client.on(events.NewMessage(chats=telegram_channels, func=filter))
 async def my_event_handler(event):
-    global global_ctx
     # Envia a nova mensagem para o canal do Discord
     if global_ctx:
-        await global_ctx.send(event.raw_text)
-        
+        text = format(event)
+        await global_ctx.send(text) 
 
 # Comando para iniciar a verificação de novas mensagens
-@bot.command()
+@bot.command(name='start')
 async def start_checking_messages(ctx):
     global global_ctx
     global_ctx = ctx
     await client.start()
-
 
 bot.run(discord_token)
